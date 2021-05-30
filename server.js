1029314edb5e3cd274338ec1306c62b4b00781e4
Prakhar Jain <theprakharjain@gmail.com>
2021-05-30 20:51:21 +0530

    made changes to server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const express = require ("express")
 const mongoose = require('mongoose')
 
-require('./models/Job');
-require('./models/Application');
-require('./models/User');
+require('./models/job');
+require('./models/application');
+require('./models/user');
 
 const app = express()
 
